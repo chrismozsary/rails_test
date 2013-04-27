7a348cfc9e3d607fd5183b075d88ac456d8edf0f
chrismozsary <christopher@mozsary.com>
2013-04-27 15:55:29 -0400

    Add user input forms

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,29 @@
 class StaticPagesController < ApplicationController
 	def welcome
 		@math = 10 * 10
+		@last_user_name = User.last.name
+		@last_user_email = User.last.email
+
+		@new_user = User.new
+
 	end
+
+def create
+
+	puts "*** *** *** *** *** ***"
+	puts params[:user]
+	puts "*** *** *** *** *** ***"
+
+@new_user= User.new(params[:user])
+
+if @new_user.save
+	flash[:success] = "welcome home, young banana."
+	redirect_to root_url
+else
+	flash[:error] = "clearly you fail at the internet."
+	redirect_to root_url
+end
+end	
+
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 RailsTest::Application.routes.draw do
   root :to => "static_pages#welcome"
   # get "/" => "static_pages#welcome"
+
+  post "create" => "static_pages#create"
+
 end
